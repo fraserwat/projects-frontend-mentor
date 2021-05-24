d0ce3b0cacb6d50ea4539347e13bf5950b27c1f5
Fraser Watt <fsrwatt@gmail.com>
2021-05-24 16:01:01 +0100

    Accessibility and menu animation

--- loopstudios-landing-page/js/hamburger.js
+++ loopstudios-landing-page/js/hamburger.js
@@ -1,9 +1,22 @@
+const bodyStyle = document.body.style;
 
 function menuClick(){
     let navBar = document.querySelector('.menu');
     if (navBar.classList.contains('active')) {
         navBar.classList.remove('active')
+        resumeScroll();
     } else {
         navBar.classList.add('active')
+        preventScroll()
     }
+}
+
+function preventScroll() {
+    document.body.style.height = '100vh';
+    document.body.style.overflowY = 'hidden';
+    document.body.style.position = 'fixed';
+}
+
+function resumeScroll() {
+    document.body.style = bodyStyle;
 }
