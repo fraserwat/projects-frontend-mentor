764de0540086c09d6fda3c490abb8c7868b0f1f6
Fraser Watt <41295918+fraserwat@users.noreply.github.com>
2021-08-04 10:24:25 +0100

    positioning desktop popup

--- 01-newbie/article-preview-component/js/sh-button.js
+++ 01-newbie/article-preview-component/js/sh-button.js
@@ -1,10 +1,8 @@
-
 shareButton = () => {
-    let popup = document.getElementById('share-popup')
-    console.log(popup.classList.contains('hide'))
-    if (popup.classList.contains('hide')) {
-        popup.classList.remove('hide') } 
-    else {
-        popup.classList.add('hide')
-    }
-}
+  let popup = document.getElementById("share-popup");
+  if (popup.classList.contains("hide")) {
+    popup.classList.remove("hide");
+  } else {
+    popup.classList.add("hide");
+  }
+};
