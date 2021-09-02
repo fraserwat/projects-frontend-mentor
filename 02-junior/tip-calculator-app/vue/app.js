2eec49d4129de5f09c92b990633b6b8db6bd3a24
Fraser Watt <41295918+fraserwat@users.noreply.github.com>
2021-09-02 22:44:42 +0100

    Vue.js

---
+++ 02-junior/tip-calculator-app/vue/app.js
@@ -0,0 +1,38 @@
+Vue.createApp({
+  data() {
+    return {
+      bill: undefined,
+      people: undefined,
+      tipPercent: 10,
+      tipPercentDisplay: undefined,
+    };
+  },
+  computed: {
+    tipAmount() {
+      const amount = (this.bill || 0) * (this.tipPercent / 100),
+        people = this.people || 1;
+      return (amount / people).toFixed(2);
+    },
+    total() {
+      const amount = (this.bill || 0) + Number(this.tipAmount),
+        people = this.people || 1;
+      return Number(amount / people).toFixed(2);
+    },
+  },
+  methods: {
+    reset() {
+      (this.bill = undefined),
+        (this.people = undefined),
+        (this.tipPercentDisplay = undefined),
+        (this.tipPercent = 10);
+    },
+    resetTipField() {
+      this.tipPercentDisplay = undefined;
+    },
+  },
+  watch: {
+    tipPercentDisplay(newVal, oldVal) {
+      this.tipPercent = newVal || 10;
+    },
+  },
+}).mount("#app");
