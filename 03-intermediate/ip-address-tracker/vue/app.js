70d4a3f3115a700c61d96ec631a5138511fd055f
Fraser Watt <41295918+fraserwat@users.noreply.github.com>
2021-09-09 22:04:11 +0100

    Fixing env variables

--- 03-intermediate/ip-address-tracker/vue/app.js
+++ 03-intermediate/ip-address-tracker/vue/app.js
@@ -41,11 +41,11 @@ Vue.createApp({
         .then(() => this.updateMapData(this.longitude, this.latitude));
     },
     createMap(lat, long) {
+      const mapApi = process.env.MAPAPI;
       this.mapObject = L.map("mapid", {
         zoomControl: false,
         scrollWheelZoom: false,
       }).setView([lat, long], 12);
-      var mapApi = process.env.MAPAPI;
       L.tileLayer(
         "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
         {
