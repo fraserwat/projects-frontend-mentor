131405cd5eb5766642f57106adc941eed2917de7
Fraser Watt <41295918+fraserwat@users.noreply.github.com>
2021-09-09 22:46:59 +0100

    hardcoding apikeys

---
+++ 03-intermediate/ip-address-tracker/vue/config.js
@@ -0,0 +1,7 @@
+const TOKEN = {
+  MAPAPI:
+    "pk.eyJ1IjoiZnJhc2Vyd2F0dCIsImEiOiJja3RkN3YwamowankwMnV0ZGdtc2s5MjVkIn0.ughfUX8TcJ6P4Uo8If7o0g",
+  IPAPI: "at_lVGNMpe3igMlBfTeF9M0KmhVHUY2K",
+};
+
+export default TOKEN;
